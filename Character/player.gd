4a620ff04da414c19767a3e7c657b6b617a2345e
germanomp <germano12344@gmail.com>
2024-07-03 16:39:46 -0300

    jump animation

--- Character/player.gd
+++ Character/player.gd
@@ -1,29 +1,21 @@
 extends CharacterBody2D
 
-
 @export var speed = 200
-@export var jump_speed = -200
-@export var double_jump = -150
+@export var jump_speed = -300
 
 @onready var animated_sprite = $AnimatedSprite2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var has_double_jumped = false
 var animation_locked = false
 var direction = Vector2.ZERO 
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	else:
-		has_double_jumped = false
 
 	if Input.is_action_just_pressed("up") or Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			velocity.y = jump_speed
-		elif not has_double_jumped:
-			velocity.y = double_jump
-			has_double_jumped = true
 
 	direction = Input.get_vector("left", "right","up","down")
 	if direction:
@@ -37,14 +29,17 @@ func _physics_process(delta):
 
 func update_animation():
 	if not animation_locked:
-		if direction.x != 0:
-			animated_sprite.play("run")
-		elif direction.x == 0:
-			animated_sprite.play("idle")
+		if not is_on_floor():
+			if velocity.y < 0:
+				animated_sprite.play("jump")
+		else:
+			if direction.x != 0:
+				animated_sprite.play("run")
+			elif direction.x == 0:
+				animated_sprite.play("idle")
 			
 func update_direction():
 	if direction.x > 0:
 		animated_sprite.flip_h = false
 	elif direction.x < 0:
 		animated_sprite.flip_h = true
-
