7a0d989cdeaa2b95e21ee928c8b4270f9cd98a89
germanomp <germano12344@gmail.com>
2024-07-03 16:30:45 -0300

    animação de andar -> pular

--- Character/player.gd
+++ Character/player.gd
@@ -1,35 +1,50 @@
 extends CharacterBody2D
 
 
-@export var speed : float = 200.0
-@export var jump : float = -200.0
-@export var double_jump : float = -150.0
+@export var speed = 200
+@export var jump_speed = -200
+@export var double_jump = -150
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+@onready var animated_sprite = $AnimatedSprite2D
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var has_double_jumped : bool = false
+var has_double_jumped = false
+var animation_locked = false
+var direction = Vector2.ZERO 
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
 		has_double_jumped = false
 
-	# Handle jump.
 	if Input.is_action_just_pressed("up") or Input.is_action_just_pressed("jump"):
 		if is_on_floor():
-			velocity.y = jump
+			velocity.y = jump_speed
 		elif not has_double_jumped:
 			velocity.y = double_jump
 			has_double_jumped = true
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
+	direction = Input.get_vector("left", "right","up","down")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction.x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
+	
 	move_and_slide()
+	update_animation()
+	update_direction()
+
+func update_animation():
+	if not animation_locked:
+		if direction.x != 0:
+			animated_sprite.play("run")
+		elif direction.x == 0:
+			animated_sprite.play("idle")
+			
+func update_direction():
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+	elif direction.x < 0:
+		animated_sprite.flip_h = true
+
